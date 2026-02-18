0a64ee33ca395047410bd0490840bdfb52b89d40
许智 <xuzhi@xuzhideMacBook-Pro-2.local>
2026-02-18 11:47:13 +0800

    build(web): stop ts build artifacts from polluting src

--- apps/web/src/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { registerSW } from "virtual:pwa-register";
-import { App } from "./app";
-import "./styles.css";
-registerSW({ immediate: true });
-createRoot(document.getElementById("root")).render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { children: _jsx(App, {}) }) }));
